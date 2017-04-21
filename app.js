6423ceaeaed1e6e125d007f7b8a146560757e26a
Elijah Carino <eecarino@gmail.com>
2017-04-20 20:21:58 -0600

    console logs in app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 alert('Welcome to Git!');
+
+console.log('We are on a new branch!')
+
+var firstName = 'Elijah';
+var lastName = 'Carino';
+
+console.log('Full Name: ' + firstName + ' ' + lastName);
