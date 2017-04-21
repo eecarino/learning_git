f8e90a6869d1cf6d84a5abb0f4a38adcb0f8bcf7
Elijah Carino <eecarino@gmail.com>
2017-04-20 20:50:04 -0600

    car input with javascript

--- app.js
+++ app.js
@@ -12,3 +12,12 @@ if (age > 19)
   console.log('Not a teen')
 else
   console.log('We are a teen. We hate our parents!');
+
+var carButton = document.getElementById('get-car');
+
+carButton.addEventListener('click', function() {
+  //i need to find the input off the page
+  var carInput = document.getElementById('car');
+  //i need to console log the input value
+  console.log('carInput.value');
+});
