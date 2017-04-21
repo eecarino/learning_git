11598503590935ad86200cc4268f223dc323eef7
Elijah Carino <eecarino@gmail.com>
2017-04-20 20:30:32 -0600

    age and if else

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ console.log('We are on a new branch!')
 
 var firstName = 'Elijah';
 var lastName = 'Carino';
+var age =27;
 
 console.log('Full Name: ' + firstName + ' ' + lastName);
+
+if (age > 19)
+  console.log('Not a teen')
+else
+  console.log('We are a teen. We hate our parents!');
